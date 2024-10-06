b1549a672673471e5bbcfcd43db2e37b677d7b29
Patrizio Tardiolo Bonifazi <p.tardiolobonifazi@vivasoft.it>
2024-10-06 20:33:12 +0200

    useEffect(() => {         const source = axios.CancelToken.source(); // Crea un token di cancellazione
            const fetchData = async () => {
    NON FUNZIONA

--- src/Components/TipoUtente/listTipoUtente.tsx
+++ src/Components/TipoUtente/listTipoUtente.tsx
@@ -16,6 +16,10 @@ const ListTipoUtente: FC<ListTipoUtenteProps> = () => {
     const [listTipoUtente, setListTipoUtente] = useState<any[]>([]);
     const [hasTipoUtente, setHasTipoUtente] = useState(false);
 
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+
     const [rowsListTipoUtente, setListRowsTipoUtente] = useState<GridRowsProp>();
     const [columnsListTipoUtente, setListColumnsTipoUtente] = useState<GridColDef[]>([]);
 
@@ -49,105 +53,146 @@ const ListTipoUtente: FC<ListTipoUtenteProps> = () => {
     https://codesandbox.io/p/sandbox/64331095-cant-add-a-button-to-every-row-in-material-ui-table-forked-hwighx?file=%2Fdemo.tsx%3A27%2C6
     https://mui.com/x/react-data-grid/server-side-data/ 
     */
+   
     useEffect(() => {
-        // call api or anything
-        console.log("loaded");
-        callGetTipiUtenteAsync();
+        const source = axios.CancelToken.source(); // Crea un token di cancellazione
 
-        const rows: GridRowsProp = [
-            { id: 1, col1: "Hello", col2: "World" },
-            { id: 2, col1: "MUI X", col2: "is awesome" },
-            { id: 3, col1: "Material UI", col2: "is amazing" },
-            { id: 4, col1: "MUI", col2: "" },
-            { id: 5, col1: "Joy UI", col2: "is awesome" },
-            { id: 6, col1: "MUI Base", col2: "is amazing" }
-        ];
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const urlRootAPI = "https://localhost:44372/";
+                const API_URL = urlRootAPI + "api/TipoUtenteAsync";
+                console.log("LOADED - fetchData - URL Endpoint = " +  API_URL);
+                const response = await axios.get(API_URL, {
+                    cancelToken: source.token, // Aggiungi il token di cancellazione
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
 
-        setListRowsTipoUtente(rows);
+                console.log(response.data);
+                console.log(response.status);
+                setListTipoUtente(response.data);
+                setHasTipoUtente(true);
 
-          const columns: GridColDef[] = [
-            { field: "id", headerName: 'Id', width: 60, hideable: true },
-            { field: 'col1', headerName: 'Tipo', width: 150 },
-            { field: 'col2', headerName: 'Descrizione', width: 250 },
-            {
-                field: "actionEdit",
-                headerName: "Edit",
-                sortable: false,
-                renderCell: (params) => {
-                  const onClick = (e: { stopPropagation: () => void; }) => {
-                    e.stopPropagation(); // don't select this row after clicking
-            
-                    const api: GridApi = params.api;
-                    // const thisRow: Record<string, GridActionsCellItemProps> = {};
-                    let thisRow: Record<string, GridActionsCellItemProps> = {};
-                    api
-                      .getAllColumns()
-                      .filter((c) => c.field !== "__check__" && !!c)
-                      thisRow["actionEdit"] = params.row
-                      /* .forEach(
-                        (c) => (thisRow[c.field] = params.row)
-                      ); */
-            
-                    return gotoEdit(params.row.id); // alert(JSON.stringify(thisRow, null, 4));
-                  };
-            
-                  return <Button onClick={onClick}>Edit</Button>;
-                }
-              },
-              {
-                field: "actionView",
-                headerName: "View",
-                sortable: false,
-                renderCell: (params) => {
-                  const onClick = (e: { stopPropagation: () => void; }) => {
-                    e.stopPropagation(); // don't select this row after clicking
-            
-                    const api: GridApi = params.api;
-                    // const thisRow: Record<string, GridActionsCellItemProps> = {};
-                    let thisRow: Record<string, GridActionsCellItemProps> = {};
-                    api
-                      .getAllColumns()
-                      .filter((c) => c.field !== "__check__" && !!c)
-                      thisRow["actionView"] = params.row
-                      /* .forEach(
-                        (c) => (thisRow[c.field] = params.row)
-                      ); */
-            
-                    return gotoView(params.row.id); // alert(JSON.stringify(thisRow, null, 4));
-                  };
-            
-                  return <Button onClick={onClick}>View</Button>;
-                }
-              },
-              {
-                field: "actionDelete",
-                headerName: "Delete",
-                sortable: false,
-                renderCell: (params) => {
-                  const onClick = (e: { stopPropagation: () => void; }) => {
-                    e.stopPropagation(); // don't select this row after clicking
-            
-                    const api: GridApi = params.api;
-                    // const thisRow: Record<string, GridActionsCellItemProps> = {};
-                    let thisRow: Record<string, GridActionsCellItemProps> = {};
-                    api
-                      .getAllColumns()
-                      .filter((c) => c.field !== "__check__" && !!c)
-                      thisRow["actionDelete"] = params.row
-                      /* .forEach(
-                        (c) => (thisRow[c.field] = params.row)
-                      ); */
-            
-                      return gotoDelete(params.row.id); // alert(JSON.stringify(thisRow, null, 4));
-                  };
-            
-                  return <Button onClick={onClick}>Delete</Button>;
-                }
-              }
-          ];
-          setListColumnsTipoUtente(columns);
+                // N.B. Nello localStorage salvo la stringa
+                localStorage.setItem("datitipoutentejson", JSON.stringify(response.data));
 
-     },[]);
+                const rows: GridRowsProp = listTipoUtente;
+                /* [
+                    { id: 1, col1: "Hello", col2: "World" },
+                    { id: 2, col1: "MUI X", col2: "is awesome" },
+                    { id: 3, col1: "Material UI", col2: "is amazing" },
+                    { id: 4, col1: "MUI", col2: "" },
+                    { id: 5, col1: "Joy UI", col2: "is awesome" },
+                    { id: 6, col1: "MUI Base", col2: "is amazing" }
+                ]; */
+                setListRowsTipoUtente(rows);
+
+                // N.B. Mettere come field i dati ritornati dalla restAPI
+                const columns: GridColDef[] = [
+                    { field: "id", headerName: 'Id', width: 60, hideable: true },
+                    { field: 'tipo', headerName: 'Tipo', width: 150 },
+                    { field: 'descrizione', headerName: 'Descrizione', width: 250 },
+                    {
+                        field: "actionEdit",
+                        headerName: "Edit",
+                        sortable: false,
+                        renderCell: (params) => {
+                        const onClick = (e: { stopPropagation: () => void; }) => {
+                            e.stopPropagation(); 
+                    
+                            const api: GridApi = params.api;
+                            let thisRow: Record<string, GridActionsCellItemProps> = {};
+                            api
+                            .getAllColumns()
+                            .filter((c) => c.field !== "__check__" && !!c)
+                            thisRow["actionEdit"] = params.row
+                            
+                    
+                            return gotoEdit(params.row.id); 
+                        };
+                    
+                        return <Button onClick={onClick}>Edit</Button>;
+                        }
+                    },
+                    {
+                        field: "actionView",
+                        headerName: "View",
+                        sortable: false,
+                        renderCell: (params) => {
+                        const onClick = (e: { stopPropagation: () => void; }) => {
+                            e.stopPropagation(); // don't select this row after clicking
+                    
+                            const api: GridApi = params.api;
+                            // const thisRow: Record<string, GridActionsCellItemProps> = {};
+                            let thisRow: Record<string, GridActionsCellItemProps> = {};
+                            api
+                            .getAllColumns()
+                            .filter((c) => c.field !== "__check__" && !!c)
+                            thisRow["actionView"] = params.row
+                            /* .forEach(
+                                (c) => (thisRow[c.field] = params.row)
+                            ); */
+                    
+                            return gotoView(params.row.id); // alert(JSON.stringify(thisRow, null, 4));
+                        };
+                    
+                        return <Button onClick={onClick}>View</Button>;
+                        }
+                    },
+                    {
+                        field: "actionDelete",
+                        headerName: "Delete",
+                        sortable: false,
+                        renderCell: (params) => {
+                        const onClick = (e: { stopPropagation: () => void; }) => {
+                            e.stopPropagation(); // don't select this row after clicking
+                    
+                            const api: GridApi = params.api;
+                            // const thisRow: Record<string, GridActionsCellItemProps> = {};
+                            let thisRow: Record<string, GridActionsCellItemProps> = {};
+                            api
+                            .getAllColumns()
+                            .filter((c) => c.field !== "__check__" && !!c)
+                            thisRow["actionDelete"] = params.row
+                            /* .forEach(
+                                (c) => (thisRow[c.field] = params.row)
+                            ); */
+                    
+                            return gotoDelete(params.row.id); // alert(JSON.stringify(thisRow, null, 4));
+                        };
+                    
+                        return <Button onClick={onClick}>Delete</Button>;
+                        }
+                    }
+                ];
+                setListColumnsTipoUtente(columns);
+
+            } catch (err) {
+            if (axios.isCancel(err)) {
+                console.log('Richiesta cancellata', err.message);
+                setHasTipoUtente(false);
+                setListRowsTipoUtente([]);
+                setListColumnsTipoUtente([]);
+                // localStorage.clear();
+            } else {
+                setError('Errore durante il recupero dei dati');
+                setListRowsTipoUtente([]);
+                setListColumnsTipoUtente([]);
+            }
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();   // Chiama la chiamata
+
+        // Pulisci la chiamata axios se il componente viene smontato
+        return () => {
+            source.cancel('La chiamata è stata annullata poiché il componente è stato smontato.');
+        };
+    }, []);
 
     const callGetTipiUtenteAsync = async () => {
         // https://localhost:7182/api/TipoUtenteAsync
@@ -186,6 +231,27 @@ const ListTipoUtente: FC<ListTipoUtenteProps> = () => {
 
     return (
         <>
+
+        {loading && (       
+            <div>
+                
+                <div style={{ height: 300, width: '100%' }}>
+                    Caricamento ...
+                </div>
+            </div>
+            
+        )}
+
+        {error && (       
+            <div>
+                
+                <div style={{ height: 300, width: '100%' }}>
+                    {error}
+                </div>
+            </div>
+            
+        )}
+
         <Container maxWidth="lg">
             <CssBaseline />
             <Box
